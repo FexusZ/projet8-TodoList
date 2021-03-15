19e19d240b01da0891a82cc6321cf2a81962b374
FexusZ <jseb.1999@outlook.fr>
2021-03-15 16:59:32 +0100

    Modification du UserRepository a cause d'un erreur de syntaxe User2 -> User

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -27,7 +27,7 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
      */
     public function upgradePassword(UserInterface $user, string $newEncodedPassword): void
     {
-        if (!$user instanceof User2) {
+        if (!$user instanceof User) {
             throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
         }
 
